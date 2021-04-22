7052a1c38df6d64e9968dfd9408f76ce7749b8d4
Yasuda5160 <58543759+Yasuda5160@users.noreply.github.com>
2021-04-22 19:46:22 +0900

    dataplot

--- experimental_data/dataplot20210104.py
+++ experimental_data/dataplot20210104.py
@@ -4,15 +4,17 @@ from pandas import DataFrame, Series
 import numpy as np
 from numpy.random import randn, rand
 from matplotlib.pylab import close
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from sympy import *
 #Input Sections
-File = input('What is the name of your experiment data?: ')
+File = input('What is the name of your experimental data file?: ')
 xaxisdata = input('Which Columns do you want to use for x axis to this Data plotting?')
 yaxisdata = input('Which Columns do you want to use for y axis to this Data plotting?')
 Title = input('What is the title of your Graph?: ')
 Xlabel = input('What is the label of x axis?: ')
 Ylabel = input('What is the label of y axis?: ')
 
+#Importing basic data from a CSV-style experimental data file
 xaxisdata = int(xaxisdata)
 yaxisdata = int(yaxisdata)
 df = pd.read_csv(File)
@@ -21,7 +23,7 @@ num = len(df)
 Xstd = df[df.columns[xaxisdata]].std()
 Ystd = df[df.columns[yaxisdata]].std()
 
-# errors
+#Errors for direct observables
 err_df0 = DataFrame(Xstd/np.sqrt(num -1), index=range(num), columns=[df.columns[xaxisdata], df.columns[yaxisdata]])
 err_df1 =  DataFrame(Ystd/np.sqrt(num - 1), index=range(num), columns=[df.columns[xaxisdata], df.columns[yaxisdata]])
 
@@ -29,7 +31,7 @@ err_df1 =  DataFrame(Ystd/np.sqrt(num - 1), index=range(num), columns=[df.column
 fig, axes = plt.subplots(constrained_layout = True, facecolor = 'white')
 axes.axis([df[df.columns[xaxisdata]].min() - 40, df[df.columns[xaxisdata]].max() + 40, df[df.columns[yaxisdata]].min() - 3, df[df.columns[yaxisdata]].max() + 3])
 
-#Creating Scatter Graph of Experiment Data
+#Creating Scatter Graph of Experimental Data
 df.plot(kind='scatter', x=df.columns[xaxisdata], y=df.columns[yaxisdata], xerr=err_df0, yerr=err_df1, ax=axes,label = 'Data Plotting')
 axes.set_title(Title, fontsize = 14)
 axes.set_xlabel(Xlabel, fontsize = 13)
@@ -45,9 +47,27 @@ b = - a * df[df.columns[xaxisdata]].mean() + df[df.columns[yaxisdata]].mean()
 x1 = np.linspace(df[df.columns[xaxisdata]].min() - 20, df[df.columns[xaxisdata]].max() + 20, 100)
 l = a.values[0]
 k = b.values[0]
+errdf0 = err_df0.values[0][0]
+errdf1 = err_df1.values[0][0]
 y1 = l * x1 + k
+
+#Error Propagation Law for arbitrary functions f(x, y)
+import math
+x, y = symbols("x y")
+function = input("f(x,y)= ")
+f = eval(function)
+err_f = math.sqrt(errdf0**2 * (diff(f, x).subs([(x, df[df.columns[xaxisdata]].mean()), (y, df[df.columns[yaxisdata]].mean())]))**2 + errdf1**2 * (diff(f, y).subs([(x, df[df.columns[xaxisdata]].mean()), (y, df[df.columns[yaxisdata]].mean())]))**2)
+
+#Creating a new columns for some indirect observables
+def indirect(frame):
+    return f.subs([(x, frame.iloc[xaxisdata]), (y, frame.iloc[yaxisdata])])
+df['f(x, y)'] = df.apply(indirect, axis = 1)
 
 #Plotting on the Scatter Graph
 axes.plot(x1, y1, color = '#ff7f00', label = 'Linear Estimation')
 axes.legend()
-plt.show()
+plt.savefig("figure.svg", transparent=True)
+
+#Identification of the regression line
+x = symbols('x')
+eq = l * x + k
